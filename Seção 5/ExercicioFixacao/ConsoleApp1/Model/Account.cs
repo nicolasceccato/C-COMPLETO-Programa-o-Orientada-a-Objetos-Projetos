f9dcd411fc2b998c2b8aa531e877fcc71a3a3ae0
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-02 20:58:14 -0300

    exercicio da secao 5

---
+++ Seção 5/ExercicioFixacao/ConsoleApp1/Model/Account.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1.Model
+{
+    public class Account
+    {
+        public int AccountNumber { get; }
+        public string Name { get; set; }
+        public double Balance { get; private set; }
+
+        public Account(int accountNumber, string name)
+        {
+            AccountNumber = accountNumber;
+            Name = name;
+            Balance = 0.0;
+        }
+        public Account(int accountNumber, string name, double value) : this(accountNumber, name)
+        {
+            Deposit(value);
+        }
+
+
+        public void Deposit(double value)
+        {
+            if (value != null && value > 0)
+            {
+                Balance += value;
+            }
+        }
+        public void Withdraw(double value)
+        {
+            if (value != null && value > 0)
+            {
+                Balance = Balance - value - 5.00;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Conta: " + AccountNumber + ", Titular: " + Name + ", Saldo: $" + Balance.ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+    }
+}
---
+++ Seção 5/ExercicioFixacao/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using ConsoleApp1.Model;
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o número da conta: ");
+            int accountNumber = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Entre o titular da conta: ");
+            string accountOwner = Console.ReadLine();
+            Console.Write("Haverá depósito inicial?(S/N) ");
+            char initialDeposit = Convert.ToChar(Console.ReadLine().ToUpper());
+            Account account = null;
+
+            if (initialDeposit == 'S')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double accountInitialDeposit = Convert.ToDouble(Console.ReadLine());
+                account = new Account(accountNumber, accountOwner, accountInitialDeposit);
+            }
+            else
+            {
+                account = new Account(accountNumber, accountOwner);
+            }
+
+            Console.Write("Entre um valor para depósito: ");
+            double deposit = Convert.ToDouble(Console.ReadLine());
+            account.Deposit(deposit);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(account);
+
+
+            Console.Write("Entre um valor para saque: ");
+            double draw = Convert.ToDouble(Console.ReadLine());
+            account.Withdraw(draw);
+            Console.WriteLine("Dados da conta atualizados: ");
+            Console.WriteLine(account);
+
+
+        }
+    }
+}
