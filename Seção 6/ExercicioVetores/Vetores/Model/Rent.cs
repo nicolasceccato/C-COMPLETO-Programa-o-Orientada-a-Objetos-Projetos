3485db60bd46b15869812e32f3d31ae56273597e
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-05 17:47:30 -0300

    Exercicio vetores

---
+++ Seção 6/ExercicioVetores/Vetores/Model/Rent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetores.Model
+{
+    internal class Rent
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+       
+        public override string ToString()
+        {
+            return Name + ", " + Email;
+        }
+
+    }
+}
---
+++ Seção 6/ExercicioVetores/Vetores/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Vetores.Model;
+
+namespace Vetores // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfStudents = Convert.ToInt32(Console.ReadLine());
+
+            Rent[] rents = new Rent[10];
+
+
+            for (int i = 0; i < numberOfStudents; i++)
+            {
+                string studentName = Console.ReadLine();
+                string studentEmail = Console.ReadLine();
+                int room = Convert.ToInt32(Console.ReadLine());
+                rents[room] = new Rent { Name = studentName, Email = studentEmail };
+            }
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (rents[i] != null)
+                {
+                    Console.WriteLine(i + ": " + rents[i]);
+                }
+            }
+        }
+    }
+}
