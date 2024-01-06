01000abb41cfb67fad06c1eec51c3fc1b9430ae4
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-06 15:37:42 -0300

    Exercicio de Listas

---
+++ Seção 6/ExercicioListas/Listas/Model/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Listas.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; private set; }
+
+        public Employee(int id, string name, double salary) 
+        { 
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void increaseSalary(double percentage)
+        {
+            Salary = Salary * (1 + percentage/100);
+        }
+
+        public override string ToString()
+        {
+            return Id + ", " + Name + ", " + Salary.ToString("F2");
+        }
+    }
+}
---
+++ Seção 6/ExercicioListas/Listas/Program.cs
@@ -0,0 +1,57 @@
+﻿using Listas.Model;
+using System;
+
+namespace Listas // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Console.Write("How many employees will be registered? ");
+            int numberOfEmployees = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < numberOfEmployees; i++)
+            {
+                Console.WriteLine("Employee #" + (i+1) + ":");
+                
+                Console.Write("Id: ");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: ");
+                double salary = Convert.ToDouble(Console.ReadLine());
+
+                employees.Add(new Employee(id, name, salary));
+
+            }
+
+            Console.Write("Enter the employee that will have salary increase: ");
+            int increaseId = Convert.ToInt32(Console.ReadLine());
+
+            bool idExist = employees.Any(x => x.Id == increaseId);
+
+            
+            if (idExist)
+            {
+
+                Console.Write("Enter the porcentage: ");
+                double increasePorcentage = Convert.ToDouble(Console.ReadLine());
+                Employee employee = employees.Find(x => x.Id == increaseId);
+                employee.increaseSalary(increasePorcentage);
+            
+            }
+            else
+            {
+                Console.WriteLine("This id does not exist!");
+            }
+            
+            Console.WriteLine("Updated list of employees:");
+            foreach(Employee e in employees)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
