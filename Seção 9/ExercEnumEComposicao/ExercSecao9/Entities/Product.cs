d345f575160c3a0f5a28396f6ffd3dc19ae6dd6a
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-08 17:51:46 -0300

    Classe Product

---
+++ Seção 9/ExercEnumEComposicao/ExercSecao9/Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace ExercSecao9.Entities;
+
+public class Product
+{
+    public string name { get; set; }
+    public double price { get; set; }
+
+    public Product(string name, double price)
+    {
+        this.name = name;
+        this.price = price;
+    }
+
+}
