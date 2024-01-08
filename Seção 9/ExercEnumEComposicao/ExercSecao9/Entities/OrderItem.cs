5717ceceae32e6b24c06c8e8f71e9dcbe3b3730d
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-08 17:51:12 -0300

    Classe OrderItem

---
+++ Seção 9/ExercEnumEComposicao/ExercSecao9/Entities/OrderItem.cs
@@ -0,0 +1,20 @@
+﻿namespace ExercSecao9.Entities;
+
+public class OrderItem
+{
+    public int quantity { get; set; }
+    public double price { get; set; }
+
+    public Product Product { get; set; }
+
+    public OrderItem(int quantity, double price)
+    {
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public double SubTotal()
+    {
+        return price * quantity;
+    }
+}
