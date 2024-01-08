bda72cb88ad7336d8bbbe257811431a514d98fb4
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-08 17:50:25 -0300

    classe Cliente

---
+++ Seção 9/ExercEnumEComposicao/ExercSecao9/Entities/Client.cs
@@ -0,0 +1,9 @@
+﻿namespace ExercSecao9.Entities;
+
+public class Client
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public DateTime BirthDate { get; set; }
+
+}
