8728b16c48c8b878a44cdc08763053c25bed8f36
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-10 17:03:53 -0300

    update(project) - creating the Product class

---
+++ Seção 10/HerancaEPolimorfismo/HerancaEPolimorfismo/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace HerancaEPolimorfismo.Entities;
+
+public class Product
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+
+    public Product()
+    {
+    }
+
+    public Product(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+    public virtual string PriceTag()
+    {
+        return Name + " $ " + Price.ToString("F2");
+    }
+}
