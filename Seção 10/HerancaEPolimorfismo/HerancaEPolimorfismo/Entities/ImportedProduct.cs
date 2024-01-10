6b271820d95eceeab64c32692e2fac1065ecb090
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-10 17:04:31 -0300

    update(project) - creating the ImportedProduct class

---
+++ Seção 10/HerancaEPolimorfismo/HerancaEPolimorfismo/Entities/ImportedProduct.cs
@@ -0,0 +1,25 @@
+﻿namespace HerancaEPolimorfismo.Entities;
+
+public class ImportedProduct : Product
+{
+    public double CustomsFee { get; set; }
+
+    public ImportedProduct()
+    {
+    }
+
+    public ImportedProduct(string name, double price, double customsFee) : base(name, price)
+    {
+        CustomsFee = customsFee;
+    }
+
+    public double TotalPrice()
+    {
+        return CustomsFee + Price;
+    }
+
+    public override string PriceTag()
+    {
+        return Name + " $ " + TotalPrice().ToString("F2") + " (Customs fee: $ " + CustomsFee + ")";
+    }
+}
