cab6b6ca37d3f48883041325eb32ac48049200e8
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-10 17:04:17 -0300

    update(project) - creating the UsedProduct class

---
+++ Seção 10/HerancaEPolimorfismo/HerancaEPolimorfismo/Entities/UsedProduct.cs
@@ -0,0 +1,21 @@
+﻿namespace HerancaEPolimorfismo.Entities;
+
+public class UsedProduct : Product
+{
+    public DateTime ManufactureDate { get; set; }
+
+    public UsedProduct()
+    {
+    }
+
+    public UsedProduct(string name, double price, DateTime manufactureDate) : base(name, price)
+    {
+        ManufactureDate = manufactureDate;
+    }
+
+    public override string PriceTag()
+    {
+        return Name + " (used) $ " + Price.ToString("F2") + " (Manufacture date: " +
+               ManufactureDate.ToString("dd/MM/yyyy") + ")";
+    }
+}
