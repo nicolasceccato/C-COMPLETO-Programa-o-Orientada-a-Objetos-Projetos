9fc8f8ab0a0f5b32c7634198995cdd37588e8041
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-11 12:08:50 -0300

    update(project) - creating the class TaxPayer

---
+++ Seção 10/MetodosAbstratos/MetodosAbstratos/Entities/TaxPayer.cs
@@ -0,0 +1,15 @@
+﻿namespace MetodosAbstratos.Entities;
+
+public abstract class TaxPayer
+{
+    public string Nome { get; set; }
+    public double AnnualIncome { get; set; }
+
+    public TaxPayer(string nome, double annualIncome)
+    {
+        Nome = nome;
+        AnnualIncome = annualIncome;
+    }
+
+    public abstract double TotalToPay();
+}
