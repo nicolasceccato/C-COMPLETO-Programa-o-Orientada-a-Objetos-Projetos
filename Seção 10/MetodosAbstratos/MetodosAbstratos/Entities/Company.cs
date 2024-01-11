407d2a435c29b3024011e8ea1694e75c4e8fb46f
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-11 12:09:48 -0300

    update(project) - creating the class Company

---
+++ Seção 10/MetodosAbstratos/MetodosAbstratos/Entities/Company.cs
@@ -0,0 +1,26 @@
+﻿namespace MetodosAbstratos.Entities;
+
+public class Company : TaxPayer
+{
+    public int NumberOfEmployees { get; set; }
+
+    public Company(int numberOfEmployees, string nome, double annualIncome) : base(nome, annualIncome)
+    {
+        NumberOfEmployees = numberOfEmployees;
+    }
+
+    public override double TotalToPay()
+    {
+        double value = 0;
+        if (NumberOfEmployees > 10)
+        {
+            value = 0.14 * AnnualIncome;
+        }
+        else
+        {
+            value = 0.16 * AnnualIncome;
+        }
+
+        return value;
+    }
+}
