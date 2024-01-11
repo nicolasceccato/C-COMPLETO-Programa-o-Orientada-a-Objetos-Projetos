cae35ea824a4cb19824cf0301151e542361e521e
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-11 12:09:20 -0300

    update(project) - creating the class Person

---
+++ Seção 10/MetodosAbstratos/MetodosAbstratos/Entities/Person.cs
@@ -0,0 +1,31 @@
+﻿namespace MetodosAbstratos.Entities;
+
+public class Person : TaxPayer
+{
+    public double HealthSpending { get; set; }
+
+    public Person(string nome, double annualIncome, double healthSpending) : base(nome, annualIncome)
+    {
+        HealthSpending = healthSpending;
+    }
+
+    public override double TotalToPay()
+    {
+        double value = 0;
+        if (AnnualIncome < 20000)
+        {
+            value = 20000 * 0.15;
+        }
+        else
+        {
+            value = 20000 * 0.25;
+        }
+
+        if (HealthSpending > 0)
+        {
+            value -= (HealthSpending * 0.5);
+        }
+
+        return value;
+    }
+}
