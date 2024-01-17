59c0fdef1cff9bcd12f1b5d5166ad98f1771dbed
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-17 14:17:03 -0300

    update(project) - implementando logica de xequemate

--- Seção 12/Xadrez/Xadrez/Program.cs
+++ Seção 12/Xadrez/Xadrez/Program.cs
@@ -42,6 +42,9 @@ namespace xadrez_console
                         Console.ReadLine();
                     }
                 }
+
+                Console.Clear();
+                Tela.imprimirPartida(partida);
             }
             catch (TabuleiroException e)
             {
--- Seção 12/Xadrez/Xadrez/Tela.cs
+++ Seção 12/Xadrez/Xadrez/Tela.cs
@@ -13,11 +13,19 @@ namespace xadrez_console
             imprimirPecasCapturadas(partida);
             Console.WriteLine();
             Console.WriteLine("Turno: " + partida.turno);
-            Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
-            Console.WriteLine();
-            if (partida.xeque)
+            if (!partida.terminada)
             {
-                Console.WriteLine("XEQUE!");
+                Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+                Console.WriteLine();
+                if (partida.xeque)
+                {
+                    Console.WriteLine("XEQUE!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("XEQUEMATE!");
+                Console.WriteLine("Vencedor: " + partida.jogadorAtual);
             }
         }
 
