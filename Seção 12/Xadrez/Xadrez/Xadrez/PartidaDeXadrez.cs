cd6866cda3ec3bd9bf6fc7b9b80685d8c4a9bcd8
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-17 14:19:00 -0300

    fix(Peca) - mudando nome do metodo para movimentoPossivel

--- Seção 12/Xadrez/Xadrez/Xadrez/PartidaDeXadrez.cs
+++ Seção 12/Xadrez/Xadrez/Xadrez/PartidaDeXadrez.cs
@@ -103,7 +103,7 @@ namespace xadrez
 
         public void validarPosicaoDestino(Posicao origem, Posicao destino)
         {
-            if (!tab.peca(origem).podeMoverPara(destino))
+            if (!tab.peca(origem).movimentoPossivel(destino))
             {
                 throw new TabuleiroException("Posicao de destino invalida!");
             }
