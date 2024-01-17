8096eb0189322a367ebf74630df3173fa1332ae7
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-17 15:31:01 -0300

    update(project) - promocao

--- Seção 12/Xadrez/Xadrez/Xadrez/PartidaDeXadrez.cs
+++ Seção 12/Xadrez/Xadrez/Xadrez/PartidaDeXadrez.cs
@@ -94,6 +94,23 @@ namespace xadrez
                 throw new TabuleiroException("Voce nao pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+            // promocao
+            if (p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+            {
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -113,7 +130,6 @@ namespace xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
 
             // enpassant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
