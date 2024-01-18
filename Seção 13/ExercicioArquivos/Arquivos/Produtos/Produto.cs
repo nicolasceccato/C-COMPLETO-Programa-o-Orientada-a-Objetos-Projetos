fb7f7fa81c1e67745e63bc3b3fb71cdadfb9199b
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-18 10:33:25 -0300

    update(project) - exercicio de arquivos

---
+++ Seção 13/ExercicioArquivos/Arquivos/Produtos/Produto.cs
@@ -0,0 +1,21 @@
+﻿namespace Produtos;
+
+public class Produto
+{
+    public string Nome { get; set; }
+    public double Preco { get; set; }
+    public int Quantidade { get; set; }
+
+    public Produto(string nome, double preco, int quantidade)
+    {
+        Nome = nome;
+        Preco = preco;
+        Quantidade = quantidade;
+    }
+
+
+    public double PrecoTotal()
+    {
+        return Preco * Quantidade;
+    }
+}
---
+++ Seção 13/ExercicioArquivos/Arquivos/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using Produtos;
+
+namespace Arquivos
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFile =
+                @"C:\temp\sourceFile.csv";
+            string targetFile =
+                @"C:\temp\out\targetFile.csv";
+
+            List<Produto> produtos = new List<Produto>();
+
+            try
+            {
+                using (StreamReader streamReader = File.OpenText(sourceFile))
+                {
+                    while (!streamReader.EndOfStream)
+                    {
+                        string[] line = streamReader.ReadLine().Split(',');
+                        string nome = line[0];
+                        double preco = Convert.ToDouble((line[1]), CultureInfo.InvariantCulture);
+                        int quantidade = Convert.ToInt32(line[2]);
+
+                        produtos.Add(new Produto(nome, preco, quantidade));
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("ERRO: " + e.Message);
+            }
+
+            try
+            {
+                using (StreamWriter streamWriter = File.AppendText(targetFile))
+                {
+                    foreach (Produto produto in produtos)
+                    {
+                        streamWriter.WriteLine(produto.Nome + "," +
+                                               produto.PrecoTotal().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("ERRO: " + e.Message);
+            }
+        }
+    }
+}
