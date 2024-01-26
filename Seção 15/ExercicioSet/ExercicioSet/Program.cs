10460b1dd615cef8f6aa6cb93dbdfe22b022a004
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-01-26 11:04:07 -0300

    update(project) - exercicio sobre conjuntos

---
+++ Seção 15/ExercicioSet/ExercicioSet/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ExercicioSet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> students = new HashSet<int>();
+
+            Console.WriteLine("How many students for course A?");
+            int numberA = Convert.ToInt32(Console.ReadLine());
+            addStudent(students, numberA);
+
+            Console.WriteLine("How many students for course B?");
+            int numberB = Convert.ToInt32(Console.ReadLine());
+            addStudent(students, numberB);
+
+            Console.WriteLine("How many students for course C?");
+            int numberC = Convert.ToInt32(Console.ReadLine());
+            addStudent(students, numberC);
+
+            Console.WriteLine("Total students: " + students.Count);
+        }
+
+        static void addStudent(HashSet<int> students, int quantity)
+        {
+            for (int i = 0; i < quantity; i++)
+            {
+                int studentId = Convert.ToInt32(Console.ReadLine());
+                students.Add(studentId);
+            }
+        }
+    }
+}
