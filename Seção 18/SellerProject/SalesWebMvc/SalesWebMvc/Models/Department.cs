e00e527de5667f0e291f12af8425524827d4db3a
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-02-02 11:44:32 -0300

    update(project) - adicionando as outras entidades e fazendo um novo migration

--- Seção 18/SellerProject/SalesWebMvc/SalesWebMvc/Models/Department.cs
+++ Seção 18/SellerProject/SalesWebMvc/SalesWebMvc/Models/Department.cs
@@ -9,5 +9,25 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department()
+        {
+
+        }
+
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+        public double TotalSales(DateTime inital, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales(inital, final));
+        }
     }
 }
