35a6c05a3e00bfe6cd82f7b3154ca418b250c2e3
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-02-03 12:58:35 -0300

    update(project) - selecao de departamento ao criar vendedor

--- Seção 18/SellerProject/SalesWebMvc/SalesWebMvc/Services/DepartmentService.cs
+++ Seção 18/SellerProject/SalesWebMvc/SalesWebMvc/Services/DepartmentService.cs
@@ -7,25 +7,18 @@ using System.Threading.Tasks;
 
 namespace SalesWebMvc.Services
 {
-    public class SellerService
+    public class DepartmentService
     {
-
         private readonly SalesWebMvcContext _context;
 
-        public SellerService(SalesWebMvcContext context)
+        public DepartmentService(SalesWebMvcContext context)
         {
             _context = context;
         }
 
-        public List<Seller> FindAll()
+        public List<Department> FindAll()
         {
-            return _context.Seller.ToList();
+            return _context.Department.OrderBy(x => x.Name).ToList();
         }
-        public void Insert(Seller obj)
-        {
-            _context.Add(obj);
-            _context.SaveChanges();
-        }
-
     }
 }
